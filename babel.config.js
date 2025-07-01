4bcb669ff0a6f44095dcb05e39dcd6d1cf07a74d
lucaserib <lucasemanuelribeiro@hotmail.com>
2025-07-01 10:12:25 -0300

    fix: corrigir configuração do expo-build-properties
    - Envolver plugin em colchetes com configuração correta
    - Desabilitar new architecture para Android e iOS
    - Atualizar versão para 1.0.8 versionCode 10

--- babel.config.js
+++ babel.config.js
@@ -13,6 +13,9 @@ module.exports = function (api) {
           extensions: ['.js', '.jsx', '.ts', '.tsx'],
         },
       ],
+      ...(process.env.NODE_ENV === 'production'
+        ? [['transform-remove-console', { exclude: ['error', 'warn'] }]]
+        : []),
     ],
   };
 };
--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,11 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
 
-const config = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname, {
+  isCSSEnabled: true,
+});
 
 module.exports = withNativeWind(config, {
   input: './app/styles/global.css',
-  configPath: './tailwind.config.js',
+  inlineRem: 16,
 });
