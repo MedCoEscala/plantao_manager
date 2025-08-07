ac2b954ef78fa2f4c7fcb74a7e21c807b9f6d0fe
Lucas Emanuel  Pereira Ribeiro <lucasemanuelpereiraribeiro@Lucass-MacBook-Air.local>
2025-08-07 13:16:33 -0300

    atualizando backend

--- backend/src/shift-templates/shift-template.module.ts
+++ backend/src/shift-templates/shift-template.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ShiftTemplatesController } from './shift-templates.controller';
 import { ShiftTemplatesService } from './shift-templates.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [ShiftTemplatesController],
   providers: [ShiftTemplatesService],
   exports: [ShiftTemplatesService],
